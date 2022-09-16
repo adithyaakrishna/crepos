2ef2a954e0a21b125e3d58adc6c18bd00d22318e
Adithya Krishna <adikrish@redhat.com>
2022-09-16 11:06:20 +0530

    WIP: Added Comment Issue Functionality
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

---
+++ src/AddIssueComment.js
@@ -0,0 +1,14 @@
+import { issueCommentsList } from "./IssueConstants";
+
+const postIssueComments = async (issueComments) => {
+    try{
+        await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', issueComments)
+    } catch(e){
+        console.log(`❌ - Oops! Couldn't Post Issue Comment For ${issueComments.issue_number}. Please try again!`)
+    }
+}
+
+export const addIssueComment = async () =>{
+    issueCommentsList.forEach(issues => postIssueComments(issues));
+}
+
---
+++ src/IssueConstants.js
@@ -0,0 +1,37 @@
+// TODO: WIP
+
+import { organizationName, repositoryName, orgContactPerson } from './RepoCreator';
+
+
+export const issueCommentsList = [
+    {
+        owner: organizationName,
+        repo: repositoryName,
+        issue_number: '23',
+        body: `${orgContactPerson} a new repository has been created at https://www.github.com/rh-impact/pl-imageio`
+    },
+    {
+        owner: organizationName,
+        repo: repositoryName,
+        issue_number: '36',
+        body: `${orgContactPerson} a new repository has been created at https://www.github.com/rh-impact/pl-timestampmaker`
+    },
+    {
+        owner: organizationName,
+        repo: repositoryName,
+        issue_number: '37',
+        body: `${orgContactPerson} a new repository has been created at https://www.github.com/rh-impact/pl-jpegoptim`
+    },
+    {
+        owner: organizationName,
+        repo: repositoryName,
+        issue_number: '39',
+        body: `${orgContactPerson} a new repository has been created at https://www.github.com/rh-impact/pl-pngquant`
+    },
+    {
+        owner: organizationName,
+        repo: repositoryName,
+        issue_number: '42',
+        body: `${orgContactPerson} a new repository has been created at https://www.github.com/rh-impact/pl-optipng`
+    }
+]
